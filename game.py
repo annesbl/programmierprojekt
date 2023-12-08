5846e4c15a84c9d2a0545529b149bbe58c6d453a
annesbl <annesoballa@icloud.com>
2023-12-08 12:34:42 +0100

    hh

---
+++ game.py
@@ -0,0 +1,21 @@
+class Game:
+  def __init__(self, board, player1, player2, m=5, n=5, k=4):
+    self.board = board
+    self.player1  = player1
+    self.player2 = player2
+    self.m = m
+    self.n = n
+    self.k = k
+    #initialize_game #erstellt alle nötigen objekte und setzt spielmodi
+    #start_game   #methode die eigentlichen spielablauf initialisiert
+
+    def game_loop():
+       # while true:
+            pass
+            
+
+    def check_draw(self, display):
+        pass#spielzug möglich?
+    
+  #check_win()  #spieler gewonnen?
+  #check_gameover() #spielfeld voll?
