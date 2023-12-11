b710e4b67207ea4c5ab3bdfd58dbb2f42c238b9a
julnas <jule.nassenstein@study.hs-duesseldorf.de>
2023-12-11 11:07:08 +0100

    start of player class

--- player.py
+++ player.py
@@ -1,1 +1,11 @@
 #player class
+
+class Player:
+    def __init__(self, name, player_number):
+        self.name = name
+        self.player_number = player_number
+
+
+
+    def make_move (board):
+        pass
