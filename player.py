55846b07c8d011a8cbc564ea3b254aeebeb2239f
julnas <jule.nassenstein@study.hs-duesseldorf.de>
2023-12-19 10:44:03 +0100

    make_move Methode

--- player.py
+++ player.py
@@ -1,11 +1,18 @@
 #player class
+#imports
+import sys
+import PyQt5.QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout, QPushButton
+from PyQt5.QtGui import QColor
+
 
 class Player:
     def __init__(self, name, player_number):
         self.name = name
         self.player_number = player_number
 
-
-
-    def make_move (board):
-        pass
+    def make_move(self, button):
+        if self.player_number == 1:
+            button.setText("X")
+        else:
+            button.setText("O")
