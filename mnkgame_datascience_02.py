3d3727a1b2acf7fb4d5a4c861d3ca81d05d648fa
BelizSnl <belizsenol@gmail.com>
2024-01-25 15:28:24 +0100

    nur noch KIs

--- mnkgame_datascience_02.py
+++ mnkgame_datascience_02.py
@@ -70,10 +70,10 @@ class Board(QMainWindow):                #beliz
         
         
 class Player:                                  #jule
-    def __init__(self, name, symbol):
+    def __init__(self, name, symbol):    #initialisierungen 
         self.name = name
         self.symbol = symbol   
-        self.is_zufallski = False
+        self.is_zufallski = False       
         self.is_einfacheki = False
         self.is_komplexeki = False
         
@@ -110,7 +110,7 @@ class KomplexeKI(Player):
 
 
     
-class Game:                                    
+class Game:                                                          
     def __init__(self, m, n, k, player1, player2):                                    
         self.player1 = player1
         self.player2 = player2
@@ -147,15 +147,16 @@ class Game:
             
             #überprüfung: Spielende?
             winner = self.check_winner()    #wenn von check winner ein symbol zurückgegeben wird, wurde das spiel von diesem symbol gewonnen.
+            
             #ausgabe gewinner (falls vorhanden)
             if winner:
-                print(f"{self.current_player.name} wins!")
-                self.board.close()  #Schließt das Fenster
-            #gleichstand
-            elif self.is_board_full() == True:
-                print("No one winns")
                 self.board.close()  #Schließt das Fenster
             
+            #gleichstand
+            elif self.is_board_full() == True:
+                self.board.close()  #Schließt das Fenster
+            
+            #kein gewinner
             elif winner == None:
             
                 #spielerwechsel (falls kein gewinner/gleichstand vorhanden)
@@ -182,28 +183,29 @@ class Game:
           symbol = self.get_symbol(i, r)
           if symbol != "":
               
+            #in den folgenden 4 fällen wurde die gleiche logik angewendet, weshalb nur eins der 4 besipiele näher erläutert werden
             #horizontal
-                count_h = 0                              #Anzahl der gleichen symbole (nebeneinandeer) in einer Reihe zählen
-                for c in range(r, self.board.n):
-                    if self.get_symbol(i, c) == symbol:
-                        count_h += 1                     #wird ein
-                        if count_h >= self.board.k:
-                            return symbol
+                count_h = 0                              #Anzahl der gleichen symbole (nebeneinandeer) in einer Reihe zählen bzw speichern
+                for c in range(r, self.board.n):         #das spielfeld horizontal durchgehen (nur zeilen durchgehen)
+                    if self.get_symbol(i, c) == symbol:  #gucken, ob das aktuelle symbol zu finden ist
+                        count_h += 1                     #ist das symbol in der reihe! -> +1 auf count_h addiren
+                        if count_h >= self.board.k:      #sind mehr oder genau k symbole nebeneinander (count_h grösser gleich k), so gewonnen 
+                            return symbol                #gewonnenes symbol wird zurück gegeben
                     else:
-                        break
+                        break                            #nicht mindestens k gleiche symbole nebneinander
                     
             #vertikal
-                count_v = 0
+                count_v = 0                              
                 for c in range(i, self.board.m):
-                    if self.get_symbol(c, r) == symbol:
-                        count_v += 1
-                        if count_v >= self.board.k:
-                            return symbol
+                    if self.get_symbol(c, r) == symbol:  
+                        count_v += 1                     
+                        if count_v >= self.board.k:      
+                            return symbol                
                     else:
-                        break
+                        break                            
                     
             # Diagonal absteigend (\)
-                count_d1 = 0
+                count_d1 = 0                             
                 for c in range(self.board.k):
                     if i + c < self.board.m and r + c < self.board.n and self.get_symbol(i + c, r + c) == symbol:
                         count_d1 += 1
@@ -259,8 +261,6 @@ def play_game(game):
     #gewinner zurückgeben
     return winner
 
-
-
 #Ausführen                                              beliz
 if __name__ == "__main__":
     app = QApplication(sys.argv)
@@ -271,21 +271,23 @@ if __name__ == "__main__":
     player_zufallski = ZufallsKI("Zufalls KI", "o", None)
     player_zufallski2 = ZufallsKI("Zufalls KI 2", "x", None)
     #player_einfacheki = EinfacheKI("Einfache KI", "o", None)
+    #player_einfacheki2 = EinfacheKI("Einfache KI 2", "x", None)
     #player_komplexeki = KomplexeKI("Einfache KI", "o", None)
+    #player_komplexeki2 = KomplexeKI("Einfache KI 2", "x", None)
     
-    #Player1 und Player2 definieren
+    #Player1 und Player2 wählen (2 der oben gennanten namen wählen - auf "x" und "o" achten)
     player1 = player_zufallski
     player2 = player_zufallski2
     
     play_several_times = True
-    num_games = 10  # Anzahl der Spiele
+    num_games = 5  # Anzahl der Spiele
     
-    #Gewinnzählung
-    wins = {player1: 0, player2: 0, "Unentschieden": 0}
+    #Gewinnzählung in einem dictionary
+    wins = {player1.name: 0, player2.name: 0, "Unentschieden": 0}
 
 
-    if play_several_times:
-        for _ in range(num_games):
+    if play_several_times:                                           #mehrere male durchlaufen --> für Data Science Fragen
+        for _ in range(num_games):                                   #für jedes game einmal die Schleife durchlaufen
             
             #game klasse aufrufen und m,n,k,player1,player2 wählen
             game = Game(5, 5, 4, player1, player2)
@@ -294,35 +296,41 @@ if __name__ == "__main__":
             player_zufallski.game = game #zufallski
             player_zufallski2.game = game #zufallski
             #player_einfacheki.game = game #einfacheki
+            #player_einfacheki2.game = game #einfacheki
             #player_komplexeki.game = game #komplexeki
+            #player_komplexeki2.game = game #komplexeki
             
             #Spiel starten
-            winner = play_game(game)
+            winner = play_game(game)           #spiel läuft und gibt einen gewinner (x bzw o) wieder
 
-            #gewinne zählen
-            if winner == player1.name:
+            #gewinne zählen  - jede runde wird ein gewinner draufaddiert
+            if winner == player1.symbol:
                 wins[player1.name] += 1       # +1 wenn player 1 gewinnt
-            elif winner == player2.name:
+            elif winner == player2.symbol:
                 wins[player2.name] += 1       # +1 wenn player 2 gewinnt
-            else:
+            elif winner is None:
                 wins["Unentschieden"] += 1    # +1 wenn unendschieden 
 
             #Pause
             time.sleep(0.15)
             
-        # Gewinne ausgeben
-        for player, win in wins.items():
-            print(f"{player} hat {win} Spiele gewonnen")
+        
+        #Gewinne ausgeben nachdem alle spiele gelaufen sind und die Schleife verlassen wurde
+        print(wins)
+
+
             
-    else:
-        #game klasse aufrufen und m,n,k,player1,player2 wählen
+    else:                           #ein einziges Spiel
+        #game klasse aufrufen und m,n,k wählen
         game = Game(5, 5, 4, player1, player2)
         
         #KIs richtig zuweisen
         player_zufallski.game = game #zufallski
         player_zufallski2.game = game #zufallski
         #player_einfacheki.game = game #einfacheki
+        #player_einfacheki2.game = game #einfacheki
         #player_komplexeki.game = game #komplexeki
+        #player_komplexeki2.game = game #komplexeki
         
         #Spiel starten
         play_game(game)
